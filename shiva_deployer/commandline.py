4a56f54c08a2c2b86635d689c1da46fe634ca790
Erik Rose <erik@mozilla.com>
2013-08-16 19:51:23 -0400

    Had a better idea about how to call deploy.py which simplifies its implementation.

--- shiva_deployer/commandline.py
+++ shiva_deployer/commandline.py
@@ -22,3 +22,5 @@ def main():
     # Peep- or pip-installs dxr/deployment/requirements.txt
     # (We can skip the previous 2 steps if the requirements are unchanged. A pip download cache should make none of this matter much.)
     # Runs `venv/python /path/to/checkout/.../deploy.py`, which builds the project as contained in the checkout and installs it
+
+    # Better still, don't call deploy.py directly at all. I don't want to have to implement (or call a routine that implements) dispatching between the bootstrap and deploy modes in every deployment script. Instead, call python -m shiva --some-special-arg /the/deploy.py, and let it twiddle sys.path, import the deploy script, and call bootstrap() or deploy() within it. That way, all a deploy script author has to do is implement those 2 callables.
