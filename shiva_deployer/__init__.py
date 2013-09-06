4fe873590e504caddb152b5aff07da259e3d0b01
Erik Rose <erik@mozilla.com>
2013-09-06 17:25:20 -0400

    Make `python -m shiva_the_deployer` work again.

--- shiva_deployer/__init__.py
+++ shiva_deployer/__init__.py
@@ -3,3 +3,9 @@
 from shiva_deployer.deployment import AbstractDeployment
 from shiva_deployer.exceptions import ShouldNotDeploy
 from shiva_deployer.tools import cd, run, nonblocking_lock
+
+
+if __name__ == '__main__':
+    from sys import exit
+    from shiva_deployer.commandline import main
+    exit(main())
